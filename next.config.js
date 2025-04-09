0ba79c400c7addff60db652f490079c5c4fbe74a
samirswe <adnansamir.d@gmail.com>
2025-04-09 14:34:45 +0600

    update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
+    unoptimized: true,
     domains: ['cdn.pixabay.com'],
+  },
+  // Add compatibility with older Node.js versions
+  experimental: {
+    esmExternals: 'loose',
   },
 }
 
